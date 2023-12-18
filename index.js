45dcd0bbe14c1d57e1d9ede4f9f321c9f04e9325
elyahu631 <eanavi363@gmail.com>
2023-12-18 14:54:14 +0200

    create routes

--- index.js
+++ index.js
@@ -1,22 +1,55 @@
 require('dotenv').config();
 const express = require('express');
-const cors = require('cors');
+const bodyParser = require('body-parser');
+const dns = require('dns');
 const app = express();
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
-app.use(cors());
-
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/public', express.static(`${process.cwd()}/public`));
+
+// URL storage
+let urlDatabase = {};
+let shortId = 1;
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.post('/api/shorturl', function(req, res) {
+  const originalUrl = req.body.url;
+
+  // Basic URL validation
+  let urlObject;
+  try {
+    urlObject = new URL(originalUrl);
+  } catch (error) {
+    return res.json({ error: 'invalid url' });
+  }
+
+  dns.lookup(urlObject.hostname, (err) => {
+    if (err) {
+      res.json({ error: 'invalid url' });
+    } else {
+      const shortUrl = shortId;
+      urlDatabase[shortUrl] = originalUrl;
+      res.json({ original_url: originalUrl, short_url: shortUrl });
+      shortId++;
+    }
+  });
+});
+
+app.get('/api/shorturl/:shortId', function(req, res) {
+  const shortUrl = req.params.shortId;
+  const originalUrl = urlDatabase[shortUrl];
+
+  if (originalUrl) {
+    res.redirect(originalUrl);
+  } else {
+    res.json({ error: 'No short URL found for the given input' });
+  }
 });
 
 app.listen(port, function() {
