bc42662ce79238f2a41a8808ca245146a749226a
elyahu631 <eanavi363@gmail.com>
2023-12-18 16:56:08 +0200

    updat

--- index.js
+++ index.js
@@ -1,61 +1,48 @@
 require("dotenv").config();
 const express = require("express");
-const bodyParser = require("body-parser");
-const dns = require("dns");
-const validUrl = require("is-url");
+const cors = require("cors");
 const app = express();
+const bodyParser = require("body-parser");
+const isUrl = "is-url";
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
+let counter = 0;
+const shortendUrls = {};
+
+app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use("/public", express.static(`${process.cwd()}/public`));
 
-// URL storage
-let urlDatabase = {};
-let shortId = 1;
-
-app.get("/", function (req, res) {
-  res.sendFile(process.cwd() + "/views/index.html");
-});
-
 app.post("/api/shorturl", function (req, res) {
-  const originalUrl = req.body.url;
+  const url = req.body.url;
 
-  if (!validUrl(originalUrl)) {
+  if (!isUrl(url)) {
     res.send({ error: "invalid url" });
     return;
   }
-  // Basic URL validation
-  let urlObject;
-  try {
-    urlObject = new URL(originalUrl);
-  } catch (error) {
-    return res.json({ error: "invalid url" });
-  }
 
-  dns.lookup(urlObject.hostname, (err) => {
-    if (err) {
-      res.json({ error: "invalid url" });
-    } else {
-      const shortUrl = shortId;
-      urlDatabase[shortUrl] = originalUrl;
-      console.log({ original_url: originalUrl, short_url: shortUrl });
-      res.json({ original_url: originalUrl, short_url: shortUrl });
-      shortId++;
-    }
-  });
+  counter += 1;
+
+  shortendUrls[counter] = url;
+  res.send({ original_url: req.body.url, short_url: counter });
 });
 
 app.get("/api/shorturl/:id", function (req, res) {
   const shortUrl = req.params.id;
   const originalUrl = urlDatabase[shortUrl];
-  console.log(originalUrl);
-  if (originalUrl) {
-    res.redirect(originalUrl);
-  } else {
-    res.json({ error: "No short URL found for the given input" });
-  }
+  res.redirect(originalUrl);
+});
+
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
+});
+
+// Your first API endpoint
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
 });
 
 app.listen(port, function () {
